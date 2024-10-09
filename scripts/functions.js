8e9463eb433aaf39804cdd52d901917231465cc0
Rhazh <ariannetagorda@gmail.com>
2024-10-09 02:33:55 -0400

    Updated the Favicon and partially the view ncrs page

--- scripts/functions.js
+++ scripts/functions.js
@@ -296,9 +296,20 @@ function performSearch() {
                             <td>${formatDate(result.dateCreated)}</td>
                             <td>${result.ncrStatus}</td>
                             <td>
-                                <div style="display: grid; gap: 10px; grid-template-columns: 1fr 1fr">
-                                    <button onclick="detailsEntry('${result.ncrNumber}')">View</button>
-                                    <button onclick="editEntry('${result.ncrNumber}')">Edit</button>
+                                <div>
+                                    <button onclick="detailsEntry('${result.ncrNumber}')">
+                                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"/>
+                                            <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                        </svg>
+                                        View
+                                    </button>
+                                    <button onclick="editEntry('${result.ncrNumber}')">
+                                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+                                        </svg>
+                                        Edit
+                                    </button>
                                 </div>
                             </td>
                         </tr>`;
